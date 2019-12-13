87061fa3a06100ea99ec48f864e512b18eff62a4
Neha Patel <neha.r.p@gmail.com>
2019-12-13 09:49:45 -0800

    finished algo questions

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,5 +6,18 @@ Your function must utilize recursion. It cannot contain any loops.
 def count_th(word):
     
     # TBC
-    
-    pass
+    #counter for "th" initialized at 0
+    count = 0
+    # split word into individual letters
+    letters = list(word)
+    print(letters)
+    print("".join(letters))
+    if len(letters) == 1:
+        return count
+    else:
+
+    # start at the end and see if values at n == "h" and n-1 == "t"
+        #if true, then add 1 to counter
+    # keep going to index n-1 until at the beginning of the word (don't check if n is the first letter, because there is no n-1)
+
+count_th("abcthefthghith")
